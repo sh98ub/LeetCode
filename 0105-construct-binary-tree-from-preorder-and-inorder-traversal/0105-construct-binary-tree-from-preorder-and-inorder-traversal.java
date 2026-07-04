607306ccc6ed2656a00c612a99bfc49806d08b8c
Shubham Gupta <120040737+sh98ub@users.noreply.github.com>
2026-07-04 14:00:14 +0530

    Time: 2 ms (64.64%), Space: 46.5 MB (36.81%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,62 +13,36 @@
  *     }
  * }
  */
-
 class Solution {
-
-    private Map<Integer, Integer> map = new HashMap<>();
     private int[] preorder;
+           private HashMap<Integer,Integer> map=new HashMap<>();
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
 
-        this.preorder = preorder;
-
-        // Store value -> index from inorder
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
-        }
-
-        return build(
-                0,
-                preorder.length - 1,
-                0,
-                inorder.length - 1
-        );
-    }
-
-    private TreeNode build(int preStart, int preEnd,
-                           int inStart, int inEnd) {
-
-        // No nodes left
-        if (preStart > preEnd || inStart > inEnd) {
+    public TreeNode build(int preStart, int preEnd,int inStart, int inEnd){
+        if(preStart>preEnd || inStart>inEnd){
             return null;
         }
+        TreeNode root=new TreeNode(preorder[preStart]);
+        int idx=map.get(root.val);
+        int leftsize=idx-inStart;
 
-        // First preorder element is the root
-        TreeNode root = new TreeNode(preorder[preStart]);
 
-        // Find root in inorder
-        int rootIndex = map.get(root.val);
+        root.left= build(preStart+1,preStart+leftsize,inStart,idx-1);
 
-        // Number of nodes in left subtree
-        int leftSize = rootIndex - inStart;
-
-        // Build left subtree
-        root.left = build(
-                preStart + 1,
-                preStart + leftSize,
-                inStart,
-                rootIndex - 1
-        );
-
-        // Build right subtree
-        root.right = build(
-                preStart + leftSize + 1,
-                preEnd,
-                rootIndex + 1,
-                inEnd
-        );
+        root.right=build(preStart+leftsize+1, preEnd, idx+1,inEnd);
 
         return root;
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder=preorder;
+
+        int n=inorder.length;
+
+        for(int i=0;i<n;i++){
+            map.put(inorder[i],i);
+        }
+
+        return  build(0,n-1,0,n-1);
+        
     }
 }
